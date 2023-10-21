f05937d5983ee48a66e305d5ee5cda7ade54db08
devbittu <devbittu@proton.me>
2023-10-21 21:14:58 +0530

    emg

--- account/urls.py
+++ account/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("login/", views.Login.as_view(), name="login"),
+    path("logout/", views.Logout.as_view(), name="logout"),
+    path("register/", views.Register.as_view(), name="register")
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
 
 # Create your views here.
 class Login(View):
     def get(self, request):
         return render(request, "account/login.html")
+
+class Logout(View):
+    def get(self, request):
+        return render(request, "account/login.html")
+
+class Register(View):
+    def get(self, request):
+        return HttpResponse(request, "This feature id diabled")
