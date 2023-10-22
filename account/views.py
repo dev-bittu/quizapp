02f543d567f7b32f486e5b446f4f5ff688e30c4b
devbittu <devbittu@proton.me>
2023-10-22 07:50:43 +0530

    adding registration system

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
@@ -15,10 +16,9 @@ class Login(View):
         uname = request.POST.get("username", "")
         passwd = request.POST.get("password", "")
         user = authenticate(username=uname, password=passwd)
-        print(uname, passwd, user)
         if user is not None:
             login(request, user)
-            messages.success(request, "Logged in successfully")
+            messages.success(request, "Logged")
             return redirect("index")
         else:
             messages.warning(request, "Username or password is incorrect")
@@ -33,4 +33,21 @@ class Logout(View):
 
 class Register(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            messages.info(request, "You are already logged in")
+            return redirect("index")
         return render(request, "account/register.html")
+    
+    def post(self, request):
+        uname = request.POST.get("username", "")
+        passwd = request.POST.get("password", "")
+        user = authenticate(username=uname, password=passwd)
+        if user is None:
+            user = User(username=uname)
+            user.set_password(passwd)
+            user.save()
+            messages.success(request, "User created")
+            return redirect("login")
+        else:
+            messages.info(request, "User already exists.")
+            return redirect("register")
