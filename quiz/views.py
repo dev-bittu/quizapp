782ce867c0bf7ac2ea8c0fcfd6b8dd15f0afb3c4
devbittu <devbittu@proton.me>
2023-10-22 11:00:21 +0530

    added add questions feature

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Question
 from django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 class Quiz(View):
@@ -15,6 +16,7 @@ class Quiz(View):
 
 
 class AddQuestion(View):
+    login_required = True
     def get(self, request):
         return render(
             request, 
@@ -25,4 +27,23 @@ class AddQuestion(View):
         )
     
     def post(self, request):
-        pass
+        for i in range(1, settings.GLOBAL_SETTINGS["questions"]+1):
+            data = request.POST
+            q = data.get(f"q{i}", "")
+            o1 = data.get(f"q{i}o1", "")
+            o2 = data.get(f"q{i}o1", "")
+            o3 = data.get(f"q{i}o1", "")
+            o4 = data.get(f"q{i}o1", "")
+            co = data.get(f"q{i}c", "")
+            question = Question(
+                question=q,
+                option1=o1,
+                option2=o2,
+                option3=o3,
+                option4=o4,
+                correct_option=co,
+                creator=request.user
+            )
+            question.save()
+        messages.success(request, "Questions added")
+        return redirect("quiz")
