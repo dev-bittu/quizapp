edafc282b42005ae03d9aaafecbd53b8d7cf568a
devbittu <devbittu@proton.me>
2023-10-22 08:13:49 +0530

    adding question model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField(blank=False)
+    option1 = models.CharField(blank=False, max_length=150)
+    option2 = models.CharField(blank=False, max_length=150)
+    option3 = models.CharField(blank=False, max_length=150)
+    option4 = models.CharField(blank=False, max_length=150)
+    correct_option = models.CharField(max_length=1, blank=False)
+    creator = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Question({self.question}, {self.creator})"
