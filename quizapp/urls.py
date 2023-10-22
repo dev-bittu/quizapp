c0290c4e03f4cbe78cb7224fc5fecfc8138ba800
devbittu <devbittu@proton.me>
2023-10-22 06:09:33 +0530

    add index page

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", view.Index.as_view(), name="index"),
     path("account/", include("account.urls"))
 ]
