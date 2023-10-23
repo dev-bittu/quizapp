45892f88ac40891dae1c34f78684ca8b6c6a1df5
devbittu <devbittu@proton.me>
2023-10-23 14:01:49 +0530

    feat: manage, upload & results

--- management/urls.py
+++ management/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.Manage.as_view(), name="manage"),
+    path("results/", views.Results.as_view(), name="results"),
+    path("upload_questions", views.UploadQuestion.as_view(), name="upload_question")
+]
--- management/views.py
+++ management/views.py
@@ -1,3 +1,39 @@
+from django.core.exceptions import ViewDoesNotExist
 from django.shortcuts import render
+from django.utils.deprecation import RenameMethodsBase
+from django.views import View
+from django.urls import reverse
+from quiz.models import Mark
 
 # Create your views here.
+class Manage(View):
+    def get(self, request):
+        panel_options = {
+            "Upload Questions": {
+                "link": reverse("upload_question"),
+                "btntxt": "Upload"
+            },
+            "Get Results": {
+                "link": reverse("results"),
+                "btntxt": "Get"
+            }
+        }
+        return render(
+            request,
+            "management/manage.html",
+            {"panel_options": panel_options}
+        )
+
+
+class Results(View):
+    def get(self, request):
+        return render(request, "management/results.html", {"results": Mark.objects.all()})
+
+
+class UploadQuestion(View):
+    def get(self, request):
+        return render(request, "management/upload_question.html")
+
+    def post(self, request):
+        qFile = request.FILE["qFile"]
+
