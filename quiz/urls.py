1969b997f2fff947834f99d6e640c8879e1fdd58
devbittu <devbittu@proton.me>
2024-01-12 20:02:38 +0530

    move class to func & reduce code

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Quiz.as_view(), name="quiz"),
-    path("add_question/", views.AddQuestion.as_view(), name="add_question"),
-    path("result/", views.Result.as_view(), name="result"),
-    path("leaderboard/", views.Leaderboard.as_view(), name="leaderboard")
+    path("", views.quiz, name="quiz"),
+    path("add_question/", views.add_question, name="add_question"),
+    path("result/", views.result, name="result"),
+    path("leaderboard/", views.leaderboard, name="leaderboard")
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, redirect
-from django.views import View
 from .models import Question, Mark
 from django.conf import settings
 from django.contrib import messages
-from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@method_decorator(login_required, name="dispatch")
-class Quiz(View):
-    def get(self, request):
+@login_required
+def quiz(request):
+    if request.method == "GET":
         questions = Question.objects.filter(verified=True)
         return render(
             request,
@@ -17,7 +15,7 @@ class Quiz(View):
             {"questions": questions}
         )
 
-    def post(self, request):
+    else:
         mark = Mark(user=request.user, total=Question.objects.filter(verified=True).count())
         for i in range(1, Question.objects.filter(verified=True).count()+1):
             q = Question.objects.filter(pk=request.POST.get(f"q{i}", 0), verified=True).first()
@@ -25,20 +23,11 @@ class Quiz(View):
                 mark.got += 1
         mark.save()
         messages.success(request, "Marks updated")
-        return redirect("result")
+    return redirect("result")
 
-@method_decorator(login_required, name="dispatch")
-class AddQuestion(View):
-    def get(self, request):
-        return render(
-            request, 
-            "quiz/add_questions.html",
-            {
-                "questions": range(1, settings.GLOBAL_SETTINGS["questions"]+1)
-            }
-        )
-    
-    def post(self, request):
+@login_required
+def add_question(request): 
+    if request.method == "POST":
         count, already_exists = 0, 0
         for i in range(1, settings.GLOBAL_SETTINGS["questions"]+1):
             data = request.POST
@@ -66,17 +55,22 @@ class AddQuestion(View):
             messages.warning(request, f"{already_exists} questions already exists")
         messages.success(request, f"{count} questions added. Wait until admin not verify it.")
         return redirect("quiz")
+    return render(
+        request,
+        "quiz/add_questions.html",
+        {
+            "questions": range(1, settings.GLOBAL_SETTINGS["questions"]+1)
+        }
+    )
 
-@method_decorator(login_required, name="dispatch")
-class Result(View):
-    def get(self, request):
-        results = Mark.objects.filter(user=request.user)
-        return render(request, "quiz/result.html", {"results": results})
+@login_required
+def result(request):
+    results = Mark.objects.filter(user=request.user)
+    return render(request, "quiz/result.html", {"results": results})
 
-class Leaderboard(View):
-    def get(self, request):
-        return render(
-            request, 
-            "quiz/leaderboard.html", 
-            {"results": Mark.objects.all().order_by("-got")[:10]}
-        )
+def leaderboard(request):
+    return render(
+        request, 
+        "quiz/leaderboard.html", 
+        {"results": Mark.objects.all().order_by("-got")[:10]}
+    )
