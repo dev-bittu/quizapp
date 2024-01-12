b016fe0e0bbee639b43f0754bdfc9371b8740882
devbittu <devbittu@proton.me>
2024-01-12 19:48:14 +0530

    convert class to functions & remove extra codes

--- management/urls.py
+++ management/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Manage.as_view(), name="manage"),
-    path("results/", views.Results.as_view(), name="results"),
-    path("upload_questions", views.UploadQuestion.as_view(), name="upload_question"),
-    path("verifiy_questions/", views.VerifyQuestion.as_view(), name="verify_question"),
-    path("setting/", views.Setting.as_view(), name="setting")
+    path("", views.manage, name="manage"),
+    path("results/", views.results, name="results"),
+    path("verifiy_questions/", views.verify_question, name="verify_question"),
+    path("setting/", views.setting, name="setting")
 ]
--- management/views.py
+++ management/views.py
@@ -1,66 +1,41 @@
 from django.shortcuts import redirect, render
-from django.views import View
 from django.urls import reverse
 from django.conf import settings
 from django.contrib import messages
-from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
 from quiz.models import Mark, Question
 from os.path import join
 
 # Create your views here.
-@method_decorator(staff_member_required, name="dispatch")
-class Manage(View):
-    def get(self, request):
-        panel_options = {
-            "Upload Questions": {
-                "link": reverse("upload_question"),
-                "btntxt": "Upload"
-            },
-            "Verify Questions": {
-                "link": reverse("verify_question"),
-                "btntxt": "Verify"
-            },
-            "Get Results": {
-                "link": reverse("results"),
-                "btntxt": "Get"
-            }
+@staff_member_required
+def manage(request):
+    panel_options = {
+        "Verify Questions": {
+            "link": reverse("verify_question"),
+            "btntxt": "Verify"
+        },
+        "Get Results": {
+            "link": reverse("results"),
+            "btntxt": "Get"
         }
-        return render(
-            request,
-            "management/manage.html",
-            {"panel_options": panel_options}
-        )
+    }
+    return render(
+        request,
+        "management/manage.html",
+        {"panel_options": panel_options}
+    )
 
-@method_decorator(staff_member_required, name="dispatch")
-class Results(View):
-    def get(self, request):
-        return render(request, "management/results.html", {"results": Mark.objects.all()})
+@staff_member_required
+def results(request):
+    return render(request, "management/results.html", {"results": Mark.objects.all()})
 
-@method_decorator(staff_member_required, name="dispatch")
-class UploadQuestion(View):
-    def get(self, request):
-        return render(request, "management/upload_question.html")
-
-    def post(self, request):
-        qFile = request.FILES["qFile"]
-        filepath = join(settings.BASE_DIR, "upload", "questions.csv")
-        if not str(qFile).endswith(".csv"):
-            messages.warning(request, "Only CSV file allowed")
-        else:
-            with open(filepath, "wb") as f:
-                for chunk in qFile.chunks():
-                    f.write(chunk)
-            messages.success(request, "CSV file uploaded")
-        return redirect("manage")
-
-@method_decorator(staff_member_required, name="dispatch")
-class VerifyQuestion(View):
-    def get(self, request):
+@staff_member_required
+def verify_question(request):
+    if request.method == "GET":
         qs = Question.objects.filter(verified=False)
         return render(request, "management/verify_question.html", {"questions": qs})
 
-    def post(self, request):
+    else:
         count = 0
         for q, v in request.POST.items():
             if q.startswith("q") and v == "on":
@@ -75,19 +50,19 @@ class VerifyQuestion(View):
         messages.success(request, f"{count} questions added")
         return redirect("manage")
 
-@method_decorator(staff_member_required, name="dispatch")
-class Setting(View):
-    def get(self, request):
+@staff_member_required
+def setting(request):
+    if request.method == "GET":
         info = {
             "question_limit": settings.GLOBAL_SETTINGS["questions"]
         }
         return render(request, "management/setting.html", {"info": info})
 
-    def post(self, request):
+    else:
         qlimit = int(request.POST.get("qlimit", 10))
         if qlimit > 0:
             settings.GLOBAL_SETTINGS["questions"] = qlimit
-            messages.success(request, "You preference saved")
+            messages.success(request, "You preferences saved") 
         else:
             messages.warning(request, "Question limit can't be 0 or less than 0")
         return redirect("setting")
